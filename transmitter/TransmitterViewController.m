b17ed4e396df2d20b3705af67cb88f4d6ad5b0db
Stephen Birarda <commit@birarda.com>
2013-05-13 12:39:24 -0700

    use AsyncUdpSocket for communication with pairing server

--- transmitter/TransmitterViewController.m
+++ transmitter/TransmitterViewController.m
@@ -7,12 +7,16 @@
 //
 
 #import <CoreMotion/CoreMotion.h>
+#import <AsyncUdpSocket.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
 
 #import "TransmitterViewController.h"
 
 @interface TransmitterViewController ()
 
 @property (nonatomic, strong) CMMotionManager *motionManager;
+@property (nonatomic, strong) AsyncUdpSocket *transmitterSocket;
 
 @end
 
@@ -21,8 +25,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    [self setupPairingSocket];
     
     // we want updates at 60Hz
     _motionManager.deviceMotionUpdateInterval = 1 / 60.0f;
@@ -49,12 +51,67 @@
     return _motionManager;
 }
 
-- (void)setupPairingSocket {
+- (AsyncUdpSocket *)transmitterSocket {    
+    if (!_transmitterSocket) {
+        _transmitterSocket = [[AsyncUdpSocket alloc] initWithDelegate:self];
+    }
     
+    return _transmitterSocket;
+}
+
+- (NSString *)wifiIPAddress {
+    // the following is a copy-paste from
+    // http://blog.zachwaugh.com/post/309927273/programmatically-retrieving-ip-address-of-iphone
+    
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while (temp_addr != NULL) {
+            if( temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if ([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                }
+            }
+            
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    
+    // Free memory
+    freeifaddrs(interfaces);
+    
+    return address;
+}
+
+- (NSData *)pairRequestData {
+    static NSData *pairRequestData = nil;
+    
+    if (!pairRequestData) {
+        NSString *pairRequestString = [NSString stringWithFormat:@"Available iOS-Transmitter %@", [self wifiIPAddress]];
+        NSLog(@"The pair request string so far is %@", pairRequestString);
+    }
+    
+    return pairRequestData;
 }
 
 - (IBAction)pairButtonTapped:(id)sender {
+    NSString* const PAIRING_SERVER_ADDRESS = @"pairing.highfidelity.io";
+    UInt16 const PAIRING_SERVER_PORT = 7247;
     
+    [self.transmitterSocket sendData:[self pairRequestData]
+                              toHost:PAIRING_SERVER_ADDRESS
+                                port:PAIRING_SERVER_PORT
+                         withTimeout:30
+                                 tag:1];
 }
 
 @end
